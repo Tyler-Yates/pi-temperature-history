092c2e212bb77f0252009586a6536cff99e0850d
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2023-11-06 21:01:47 -0600

    More work

--- pitemp/__main__.py
+++ pitemp/__main__.py
@@ -7,7 +7,13 @@ USB_SENSOR = '/dev/ttyUSB0'
 def main():
     sensor = TemperatureSensor(UART_Adapter(USB_SENSOR))
     sensor.info()
-    print(sensor.get_temperature())
+    temperature_c = sensor.get_temperature()
+    temperature_f = _convert_c_to_f(temperature_c)
+    print(f"Temperature in F: {temperature_f}")
+
+
+def _convert_c_to_f(c: float) -> float:
+    return c * 9.0 / 5.0 + 32
 
 
 if __name__ == '__main__':
